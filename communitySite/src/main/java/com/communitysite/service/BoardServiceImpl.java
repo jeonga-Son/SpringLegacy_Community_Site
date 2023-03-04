bfda951ad30ca246ec3e75d5692d1d9115f65c8a
jeonga-Son <jjeong0106@gmail.com>
2023-03-05 02:28:10 +0900

    [feat] BoardService 구현

---
+++ communitySite/src/main/java/com/communitysite/service/BoardServiceImpl.java
@@ -0,0 +1,47 @@
+package com.communitysite.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.communitysite.domain.BoardVO;
+import com.communitysite.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+	
+	@Autowired
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		return null;
+	}
+
+}
