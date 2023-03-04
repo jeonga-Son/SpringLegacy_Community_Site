e388e9e2f4510110106a49653279b77ac53c21e5
jeonga-Son <jjeong0106@gmail.com>
2023-03-04 23:15:27 +0900

    [feat] create(insert) 구현 및 테스트

--- communitySite/src/main/java/com/communitysite/mapper/BoardMapper.java
+++ communitySite/src/main/java/com/communitysite/mapper/BoardMapper.java
@@ -6,4 +6,8 @@ import com.communitysite.domain.BoardVO;
 
 public interface BoardMapper {
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
 }
