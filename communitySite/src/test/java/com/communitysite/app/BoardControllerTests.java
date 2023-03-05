37037293d565a52f5dc09b453bf6bcad5b80d2da
jeonga-Son <jjeong0106@gmail.com>
2023-03-05 10:56:14 +0900

    [feat] BoardController 삭제 처리 및 테스트 구현

--- communitySite/src/test/java/com/communitysite/app/BoardControllerTests.java
+++ communitySite/src/test/java/com/communitysite/app/BoardControllerTests.java
@@ -67,16 +67,25 @@ public class BoardControllerTests {
 //				.getModelAndView().getModelMap());
 //	}
 	
+//	@Test
+//	public void testModify() throws Exception {
+//		String resultPage = mockMvc
+//				.perform(MockMvcRequestBuilders.post("/board/modify")
+//						.param("bno", "2")
+//						.param("title", "수정된 테스트 새글 제목")
+//						.param("content", "수정된 테스트 새글 내용")
+//						.param("writer", "user0123"))
+//				.andReturn().getModelAndView().getViewName();
+//		
+//		log.info(resultPage);
+//	}
+	
 	@Test
-	public void testModify() throws Exception {
+	public void testRemove() throws Exception {
 		String resultPage = mockMvc
-				.perform(MockMvcRequestBuilders.post("/board/modify")
-						.param("bno", "2")
-						.param("title", "수정된 테스트 새글 제목")
-						.param("content", "수정된 테스트 새글 내용")
-						.param("writer", "user0123"))
-				.andReturn().getModelAndView().getViewName();
-		
+				.perform(MockMvcRequestBuilders.post("/board/remove")
+						.param("bno", "3")
+						).andReturn().getModelAndView().getViewName();
 		log.info(resultPage);
-	}
+	} 
 }
